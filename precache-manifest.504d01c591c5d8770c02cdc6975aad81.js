03beeb9219660b517d750153074397991a214efb
Adam Czakon <adam.czakon63@gmail.com>
2019-07-18 23:09:34 +0200

    Updates

---
+++ precache-manifest.504d01c591c5d8770c02cdc6975aad81.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b15646e712ea5d010f291a84d54dbdb4",
+    "url": "/portfolio/static/media/spaceverse.b15646e7.png"
+  },
+  {
+    "revision": "c65f1bb968ddf5397f518f2234c1cf2c",
+    "url": "/portfolio/static/media/itcloud.c65f1bb9.png"
+  },
+  {
+    "revision": "22fdaa0b72bb063a74fcc36590d16b91",
+    "url": "/portfolio/static/media/githubIcon.22fdaa0b.svg"
+  },
+  {
+    "revision": "f5e2f4f3fdc8621a10b345d743c38de3",
+    "url": "/portfolio/static/media/formovie.f5e2f4f3.png"
+  },
+  {
+    "revision": "0e6bf2448cee65fe6fcaa4c0d42b593b",
+    "url": "/portfolio/static/media/background.0e6bf244.jpg"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5139d883e350db845b23",
+    "url": "/portfolio/static/js/main.c70d3d6b.chunk.js"
+  },
+  {
+    "revision": "89b240c37ce40c6b078f",
+    "url": "/portfolio/static/js/2.7ae4405b.chunk.js"
+  },
+  {
+    "revision": "5139d883e350db845b23",
+    "url": "/portfolio/static/css/main.79d3eb1e.chunk.css"
+  },
+  {
+    "revision": "5f7bd9092dfef65115cea0691e2fd1c2",
+    "url": "/portfolio/index.html"
+  }
+];
