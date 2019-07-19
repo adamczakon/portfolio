17d1a28d0fc5a6a62593b541f91229a505a9e169
Adam Czakon <adam.czakon63@gmail.com>
2019-07-19 11:44:21 +0200

    responsiveness fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ export default function Home() {
         </div>
       </div>
       <div className='home__introduction'>
-        <h1 className='heading'>My Portfolio</h1>
+        <h1 className='introduction__heading'>My Portfolio</h1>
         <p className='introduction__text'>
           Below you can see projects I've been working on.
           <br /> I've listed technologies that I used to create each project.
